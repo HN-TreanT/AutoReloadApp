58ec801495be224fa8e15ca837604429199b34a8
TreanT <hntreant23@gmail.com>
2024-08-10 01:37:00 +0700

    first commit

---
+++ AutoReloadApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AutoReloadApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            start = new Button();
+            stop = new Button();
+            label1 = new Label();
+            inputDuration = new TextBox();
+            SuspendLayout();
+            // 
+            // start
+            // 
+            start.BackColor = Color.FromArgb(0, 205, 151);
+            start.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            start.ForeColor = Color.White;
+            start.Location = new Point(223, 366);
+            start.Name = "start";
+            start.Size = new Size(160, 60);
+            start.TabIndex = 2;
+            start.Text = "start";
+            start.UseVisualStyleBackColor = false;
+            start.Click += start_Click;
+            // 
+            // stop
+            // 
+            stop.BackColor = Color.FromArgb(247, 86, 91);
+            stop.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            stop.ForeColor = Color.White;
+            stop.Location = new Point(447, 366);
+            stop.Name = "stop";
+            stop.Size = new Size(160, 60);
+            stop.TabIndex = 3;
+            stop.Text = "stop";
+            stop.UseVisualStyleBackColor = false;
+            stop.Click += stop_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 72F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(175, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(848, 217);
+            label1.TabIndex = 4;
+            label1.Text = "00:00:00";
+            // 
+            // inputDuration
+            // 
+            inputDuration.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            inputDuration.Location = new Point(692, 381);
+            inputDuration.Name = "inputDuration";
+            inputDuration.Size = new Size(278, 35);
+            inputDuration.TabIndex = 5;
+            inputDuration.TextChanged += inputDuration_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(29, 38, 48);
+            ClientSize = new Size(1178, 475);
+            Controls.Add(inputDuration);
+            Controls.Add(label1);
+            Controls.Add(stop);
+            Controls.Add(start);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button start;
+        private Button stop;
+        private Label label1;
+        private TextBox inputDuration;
+    }
+}
---
+++ AutoReloadApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Windows.Forms;
+namespace AutoReloadApp
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+        int totalSeconds; 
+        int remainingSeconds;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetForegroundWindow(IntPtr hWnd);
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputDuration.KeyPress += new KeyPressEventHandler(inputDuration_KeyPress);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize the timer
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000; // 1 second interval
+            timer.Elapsed += OnTimerEvent;
+        }
+
+        private void OnTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(async () =>
+            {
+                if (remainingSeconds > 0)
+                {
+                    remainingSeconds -= 1; // Decrease by 1 second
+                    UpdateLabel(remainingSeconds);
+                }
+                else
+                {
+                    string[] browserProcesses = { "msedge", "chrome", "firefox" };
+
+                    foreach (var processName in browserProcesses)
+                    {
+                        Process[] processes = Process.GetProcessesByName(processName);
+
+                        if (processes.Length > 0)
+                        {
+                            SetForegroundWindow(processes[0].MainWindowHandle);
+                            SendKeys.SendWait("{F5}");
+                            await Task.Delay(1000);
+                            SendKeys.SendWait("{F11}");
+                            break;
+                        }
+                    }
+
+                    remainingSeconds = totalSeconds;
+                    UpdateLabel(totalSeconds);
+              
+                }
+            }));
+        }
+
+        private void UpdateLabel(int seconds)
+        {
+            int h = seconds / 3600;
+            int m = (seconds % 3600) / 60;
+            int s = seconds % 60;
+
+            label1.Text = string.Format("{0:D2}:{1:D2}:{2:D2}", h, m, s);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            start.Enabled = false;
+            start.BackColor = Color.FromArgb(169, 169, 169); 
+            start.ForeColor = Color.FromArgb(128, 128, 128);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            start.Enabled = true;
+            start.BackColor = Color.FromArgb(0, 205, 151);
+            start.ForeColor = Color.White;
+        }
+
+        private void inputDuration_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputDuration.Text, out remainingSeconds))
+            {
+                totalSeconds = remainingSeconds;
+                UpdateLabel(remainingSeconds);
+            }
+        }
+
+        private void inputDuration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+ 
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Suppress the keypress
+            }
+        }
+
+    }
+
+}
